3080cc2f70b2721923dbc95433e748173a4f4e13
Sebastian Céspedes <sebasco0397@gmail.com>
2018-05-13 00:06:03 -0500

    [del] remove johnny-five dependency

--- dwelling-rear/server.js
+++ dwelling-rear/server.js
@@ -2,7 +2,6 @@ const app = require('express')();
 const morgan = require('morgan');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const j5 = require('johnny-five');
 var board;
 // var board = new j5.Board({
 //   repl: false
@@ -69,93 +68,93 @@ io.on('connection', function(socket) {
   /* DOORS */
 
   // check if a door is open(roll) or close(over)
-  socket.on('statusDoor', (door, fn) => {
-    var status = doorsEngine[doors[door]].value;
-    if (status == 90) {
-      fn('over');
-    } else {
-      fn('roll');
-    }
-  });
+  // socket.on('statusDoor', (door, fn) => {
+  //   var status = doorsEngine[doors[door]].value;
+  //   if (status == 90) {
+  //     fn('over');
+  //   } else {
+  //     fn('roll');
+  //   }
+  // });
 
-  // set close or open to specific door
-  socket.on('rollup-rollover', (door, status, fn) => {
-    if (status == 'over') {
-      doorsEngine[doors[door]].to(0, 3000, 10);
-      fn('roll');
-    } else {
-      doorsEngine[doors[door]].to(90, 3000, 10);
-      fn('over');
-    }
-  });
+  // // set close or open to specific door
+  // socket.on('rollup-rollover', (door, status, fn) => {
+  //   if (status == 'over') {
+  //     doorsEngine[doors[door]].to(0, 3000, 10);
+  //     fn('roll');
+  //   } else {
+  //     doorsEngine[doors[door]].to(90, 3000, 10);
+  //     fn('over');
+  //   }
+  // });
 
   /* WINDOW */
-  socket.on('wheelWindow', (status, fn) => {
-    var rotation;
-    if (status == 'down') {
-      rotation = -200;
-    } else {
-      rotation = 200;
-    }
-    const stepsRight = [[0, 0, 1, 1], [0, 1, 1, 0], [1, 1, 0, 0], [1, 0, 0, 1]];
-    const stepsLeft = [[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1], [1, 0, 0, 1]];
-    if (status == 'down') {
-      stepRotation = stepsLeft;
-    } else {
-      stepRotation = stepsRight;
-    }
-    rotation = rotation * 1.4222222222; // ajuste de 512 vueltas a 360 grados
-    rotation = Math.abs(rotation);
-    rotarVentana(rotation);
+  // socket.on('wheelWindow', (status, fn) => {
+  //   var rotation;
+  //   if (status == 'down') {
+  //     rotation = -200;
+  //   } else {
+  //     rotation = 200;
+  //   }
+  //   const stepsRight = [[0, 0, 1, 1], [0, 1, 1, 0], [1, 1, 0, 0], [1, 0, 0, 1]];
+  //   const stepsLeft = [[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1], [1, 0, 0, 1]];
+  //   if (status == 'down') {
+  //     stepRotation = stepsLeft;
+  //   } else {
+  //     stepRotation = stepsRight;
+  //   }
+  //   rotation = rotation * 1.4222222222; // ajuste de 512 vueltas a 360 grados
+  //   rotation = Math.abs(rotation);
+  //   rotarVentana(rotation);
 
-    // rotateWindow(0, Math.abs(rotation));
-    fn('ok');
-  });
+  //   // rotateWindow(0, Math.abs(rotation));
+  //   fn('ok');
+  // });
 
-  function rotateWindow(index, step) {
-    if (step > 0) {
-      step = index < 4 ? step : step - 1;
-      index = index < 4 ? index : 0;
-      for (var i = 0; i <= 3; i++) {
-        // console.log(stepRotation[index][i]);
-        board.digitalWrite(window[index], stepRotation[index][i]);
-      }
-      board.wait(5, () => {
-        // console.log('######');
-        rotateWindow(index + 1, step);
-      });
-    } else {
-      console.log('close');
-      for (var j = 0; j <= 3; j++) {
-        board.digitalWrite(window[j], 0);
-      }
-    }
-  }
+  // function rotateWindow(index, step) {
+  //   if (step > 0) {
+  //     step = index < 4 ? step : step - 1;
+  //     index = index < 4 ? index : 0;
+  //     for (var i = 0; i <= 3; i++) {
+  //       // console.log(stepRotation[index][i]);
+  //       board.digitalWrite(window[index], stepRotation[index][i]);
+  //     }
+  //     board.wait(5, () => {
+  //       // console.log('######');
+  //       rotateWindow(index + 1, step);
+  //     });
+  //   } else {
+  //     console.log('close');
+  //     for (var j = 0; j <= 3; j++) {
+  //       board.digitalWrite(window[j], 0);
+  //     }
+  //   }
+  // }
 
-  function rotarVentana(steps) {
-    for (var i = 0; i <= 3; i++) {
-      console.log(window[i], stepRotation[i][0]);
-      board.digitalWrite(window[i], stepRotation[i][0]);
-    }
-    for (var i = 0; i <= 3; i++) {
-      console.log(window[i], stepRotation[i][0]);
-      board.digitalWrite(window[i], stepRotation[i][1]);
-    }
-    for (var i = 0; i <= 3; i++) {
-      console.log(window[i], stepRotation[i][0]);
-      board.digitalWrite(window[i], stepRotation[i][2]);
-    }
-    for (var i = 0; i <= 3; i++) {
-      console.log(window[i], stepRotation[i][0]);
-      board.digitalWrite(window[i], stepRotation[i][3]);
-    }
-    setTimeout(() => {
-      steps = steps - 1;
-      if (steps > 0) {
-        rotarVentana(steps);
-      }
-    }, 100);
-  }
+  // function rotarVentana(steps) {
+  //   for (var i = 0; i <= 3; i++) {
+  //     console.log(window[i], stepRotation[i][0]);
+  //     board.digitalWrite(window[i], stepRotation[i][0]);
+  //   }
+  //   for (var i = 0; i <= 3; i++) {
+  //     console.log(window[i], stepRotation[i][0]);
+  //     board.digitalWrite(window[i], stepRotation[i][1]);
+  //   }
+  //   for (var i = 0; i <= 3; i++) {
+  //     console.log(window[i], stepRotation[i][0]);
+  //     board.digitalWrite(window[i], stepRotation[i][2]);
+  //   }
+  //   for (var i = 0; i <= 3; i++) {
+  //     console.log(window[i], stepRotation[i][0]);
+  //     board.digitalWrite(window[i], stepRotation[i][3]);
+  //   }
+  //   setTimeout(() => {
+  //     steps = steps - 1;
+  //     if (steps > 0) {
+  //       rotarVentana(steps);
+  //     }
+  //   }, 100);
+  // }
 });
 
 server.listen(4028);
